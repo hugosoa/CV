cc7670de854e90603f08eaa83192bbdedba14aad
hugosoa <119794163+hugosoa@users.noreply.github.com>
2023-03-27 08:06:38 +0200

    scroll-barre

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function toggleNav(){
     hamburgerButton.classList.toggle("active")
     navigation.classList.toggle("active")
 }
+
+window.onscroll = function(){
+    window.addEventListener("scroll", () => {
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight;
+        let position = window.scrollY;
+        let positionHauteur = document.documentElement.clientHeight;
+
+        let barre = (position / hauteur) * positionHauteur;
+
+        document.getElementById("barre").style.height = barre + "px";
+    });
+}
